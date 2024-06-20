382bf664805b5c48dc93d38dcce74e641b9cfc82
Kotla Eswar <21pa1a1256@vishnu.edu.in>
2024-06-20 16:10:59 +0530

    mm

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ menuIcon.onclick = () => {
 
 /*========== scroll sections active link ==========*/
 let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+let navLinks = document.querySelectorAll('header av a');
 
 window.onscroll = () => {
     sections.forEach(sec => {
